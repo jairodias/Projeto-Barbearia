a2fb3fa57e74cb52290040f12693007972fb95ca
Jairo <jairopereira_dias@hotmail.com>
2020-05-12 07:52:04 -0300

    salvando

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -4,19 +4,12 @@ module.exports = {
     async create(request, response){
         const {name, password, password1, email} = request.body;
     
-        if(password == password1){
             await connection('users').insert({
                 name,
                 password,
-                email,
+                email
             });
-
+                
             return response.redirect('/');
-        }else{
-            
-            const dados = await connection('users').select('*');
-
-            console.log(dados);
-        }
     }
 }
