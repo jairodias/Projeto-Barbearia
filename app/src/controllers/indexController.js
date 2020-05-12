b50b70f462185f77e744006aa324bfa8e49a71e5
Jairo <jairopereira_dias@hotmail.com>
2020-05-12 07:52:31 -0300

    retirando uma variavel

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -2,7 +2,7 @@ const connection = require('../../database/connection');
 
 module.exports = {
     async create(request, response){
-        const {name, password, password1, email} = request.body;
+        const {name, password, email} = request.body;
     
             await connection('users').insert({
                 name,
