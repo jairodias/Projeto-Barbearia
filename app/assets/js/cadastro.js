b81e47ddb69daa5f669e484c3ef0bdb5b4ad4f8d
Jairo <jairopereira_dias@hotmail.com>
2020-05-13 06:01:30 -0300

    implementando validações de senhas

--- app/assets/js/cadastro.js
+++ app/assets/js/cadastro.js
@@ -4,22 +4,61 @@ $(document).ready(function(){
         var password = $('#password').val();
         var password1 = $('#password1').val();
         var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-        
-        if(password == password1){
-            if(reg.test(email)){
-            
-            }else{
-                return false;
-            }
-        }else{
+        var senha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        var flag = true;
+        if((!senha.test(password)) || (!senha.test(password1))){
+            flag = false;
+            Swal.fire(
+                'Ops!',
+                'As senhas devem conter o mínimo de oito caracteres, pelo menos uma letra maiúscula, uma letra minúscula e um número',
+                'error'
+            )
+        }
+
+        if(!reg.test(email)){
+            flag = false;
+            Swal.fire(
+                'Ops!',
+                'Email não corresponde ao padrão xxxxx@xxxx.com',
+                'error'
+            )
+        }
+
+        if(password !== password1){
+            flag = false;
             $('#password').addClass('is-invalid');
             $('#password1').addClass('is-invalid');
 
-            return false;
+            $('#password').val('');
+            $('#password1').val('');
+            Swal.fire(
+                'Ops!',
+                'Senhas não conferem',
+                'error'
+            )
         }
-
-        $('#cadastrar').submit();
-
-        console.log(email, password, password1);
+        if(flag !== false){
+            $.ajax({
+                url: `/cadastrar`,
+                type: "POST",
+                dataType: 'json',
+                data: $("#cadastrar").serialize(),
+                success: function (json) {
+                    if (json.status === 1) {
+                        Swal.fire(
+                            'Cadastrado com sucesso!',
+                            'Você será redirecionado para a página principal',
+                            'success'
+                        ).then((result) => {
+                            if(result.value){
+                                window.location.href = '/';
+                            }
+                        });
+                    } else {
+                        alert('Ops, falha em enviar menssagem');
+                    }
+                }
+            });
+        }
     })
 });
--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -3,13 +3,16 @@ const connection = require('../../database/connection');
 module.exports = {
     async create(request, response){
         const {name, password, email} = request.body;
-    
+
             await connection('users').insert({
                 name,
                 password,
                 email
             });
                 
-            return response.redirect('/');
+            return response.json({
+                status: 1,
+                message: "Dados cadastrados com sucesso"
+            });
     }
 }
