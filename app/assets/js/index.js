6683c054d09b4ff3fe613bb009e7c2e125817ecb
Jairo <jairopereira_dias@hotmail.com>
2020-05-13 06:18:44 -0300

    aumentando o tamanho do card do input

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,17 +1,17 @@
 
 $(document).ready(function(){
 
-    $('#close').on('click', function(){
-       $('#open').removeAttr('hidden');
-       $('#close').attr('hidden', true);
+    $('.fa-eye-slash').on('click', function(){
+       $('.fa-eye').removeAttr('hidden');
+       $('.fa-eye-slash').attr('hidden', true);
        $('#password').attr('type', 'text');
+       $('#password1').attr('type', 'text');
     })
 
-    $('#open').on('click', function(){
-        $('#close').removeAttr('hidden');
-        $('#open').attr('hidden', true);
+    $('.fa-eye').on('click', function(){
+        $('.fa-eye-slash').removeAttr('hidden');
+        $('.fa-eye').attr('hidden', true);
         $('#password').attr('type', 'password');
+        $('#password1').attr('type', 'password');
     })
-
-
 })
