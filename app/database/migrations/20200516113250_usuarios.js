4f3e1d1a32ba656fbdd237c50ac7bfae3ebd04b3
Jairo <jairopereira_dias@hotmail.com>
2020-05-16 11:38:42 -0300

    criando a migrate da tabela de usuários

---
+++ app/database/migrations/20200516113250_usuarios.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('usuarios', function(table){
+    table.string('id').primary();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.string('passwordResetToken');
+    table.date('passwordResetExpires');
+    table.timestamp(true);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuarios');
+};
